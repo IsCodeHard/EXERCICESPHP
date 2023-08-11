4974a4c854fffc0dc1878c0315b0018f37e52c73
Brou <brou@192.168.50.20>
2023-08-11 12:07:23 +0000

    quote simple  pour les chaînes de caractères

--- in_array.php
+++ in_array.php
@@ -4,10 +4,10 @@
     $array = [rand(0,100),rand(0,100),rand(0,100),rand(0,100),rand(0,100),rand(0,100),rand(0,100),rand(0,100),rand(0,100),rand(0,100),rand(0,100),rand(0,100),rand(0,100),rand(0,100),rand(0,100),rand(0,100),rand(0,100),rand(0,100),rand(0,100),rand(0,100),rand(0,100),rand(0,100),rand(0,100),rand(0,100),rand(0,100),rand(0,100),rand(0,100),rand(0,100),rand(0,100),rand(0,100),rand(0,100),rand(0,100),rand(0,100),rand(0,100),rand(0,100),rand(0,100)];
 
     while ($count<3) { 
-        $search = readline("Valeur rechercher : ");
+        $search = readline('Valeur rechercher : ');
 
         if(in_array($search, $array)){
-            echo "> ".$search." < est présent dans le tableau";
+            echo '> '.$search.' < est présent dans le tableau';
             $find = True;
             break;
         }else{
@@ -20,7 +20,7 @@
     if($count>=3 && !$find){
         echo "Désolé, vous n'avez pas pu déviner\n";
         for ($i=0; $i < count($array); $i++) { 
-            echo " < ".$array[$i]." > ";
+            echo ' < '.$array[$i].' > ';
         }
     }
 ?>
